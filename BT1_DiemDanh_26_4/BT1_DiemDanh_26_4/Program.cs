d08a9d15cec7ebe97aa36d71fb1a1754f7c84137
Trần Tiến <trantien01ht@gmail.com>
2022-04-26 07:48:21 +0700

    add

---
+++ BT1_DiemDanh_26_4/BT1_DiemDanh_26_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT1_DiemDanh_26_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BT2_DiemDanh_26_4/BT2_DiemDanh_26_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT2_DiemDanh_26_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
