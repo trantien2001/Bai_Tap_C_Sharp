325ec5e3541573ee5a28a6116891e917d8ba23ab
Trần Tiến <trantien01ht@gmail.com>
2022-05-10 07:49:21 +0700

    add

--- BT_Diem_Danh_10_05/BT_Diem_Danh_10_05/Program.cs
+++ BT_Diem_Danh_10_05/BT_Diem_Danh_10_05/Program.cs
@@ -15,10 +15,10 @@ namespace BT_Diem_Danh_10_05
                 a_252[i_252] = int.Parse(Console.ReadLine());
             }
             int min_252 = a_252[0], max_252 = a_252[0];
-            // Sắp xếp 
-            for(i_252 = 0; i_252 < 10; i_252++)
+            //// Sắp xếp tăng
+            for (i_252 = 0; i_252 < 10; i_252++)
             {
-                for(j_252=i_252+1; j_252 < 10; j_252++) 
+                for (j_252 = i_252 + 1; j_252 < 10; j_252++)
                 {
                     if (a_252[i_252] > a_252[j_252])
                     {
@@ -28,15 +28,33 @@ namespace BT_Diem_Danh_10_05
                     }
                 }
             }
-
+            Console.Write("Sap xep tang dan: ");
             for (i_252 = 0; i_252 < 10; i_252++)
             {
                 Console.Write(" " + a_252[i_252]);
             }
 
+            // Sắp xếp giam
+            for (i_252 = 0; i_252 < 10; i_252++)
+            {
+                for (j_252 = i_252 + 1; j_252 < 10; j_252++)
+                {
+                    if (a_252[i_252] < a_252[j_252])
+                    {
+                        tam_252 = a_252[i_252];
+                        a_252[i_252] = a_252[j_252];
+                        a_252[j_252] = tam_252;
+                    }
+                }
+            }
+            Console.Write("\nSap xep giam dan: ");
+            for (i_252 = 0; i_252 < 10; i_252++)
+            {
+                Console.Write(" " + a_252[i_252]);
+            }
 
             // Tim min max
-            for (i_252 = 1; i_252 < 10; i_252++)
+            for (i_252 = 0; i_252 < 10; i_252++)
             {
                 if (a_252[i_252] < min_252)
                 {
@@ -47,8 +65,8 @@ namespace BT_Diem_Danh_10_05
                     max_252 = a_252[i_252];
                 }
             }
-            Console.WriteLine("Gia trị min: " + min_252);
-            Console.WriteLine("Gia trị max: " + max_252);
+            Console.WriteLine("Gia tri min: " + min_252);
+            Console.WriteLine("Gia tri max: " + max_252);
 
         }
         static void Main(string[] args)
